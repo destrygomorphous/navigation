8e869c4366b5e9b5a06f4e6cc7fbb8ad19d8d5e5
destrygomorphous <julie.kew@gmail.com>
2017-06-09 08:11:36 -0700

    implement A*

--- mapping.py
+++ mapping.py
@@ -3,6 +3,7 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+import heapq
 
 class OGrid:
     def __init__(self):
@@ -88,6 +89,79 @@ class NavigatorStraight(NavigatorRandom):
                     return 6
                 else:
                     self.direction = 0
+
+def dist(x0, y0, x1, y1):
+    return ((x0 - x1) ** 2 + (y0 - y1) ** 2) ** 0.5
+
+def reconstructPath(cameFrom, c):
+    totalPath = [c]
+    while c in cameFrom.keys():
+        c = cameFrom[c]
+        totalPath.append(c)
+    print totalPath
+    directions = []
+    for n in reversed(totalPath[:-1]):
+        if n[1] - c[1] == -1 and n[0] == c[0]:
+            directions.append(0)
+        elif n[1] - c[1] == -1 and n[0] - c[0] == 1:
+            directions.append(1)
+        elif n[1] == c[1] and n[0] - c[0] == 1:
+            directions.append(2)
+        elif n[1] - c[1] == 1 and n[0] - c[0] == 1:
+            directions.append(3)
+        elif n[1] - c[1] == 1 and n[0] == c[0]:
+            directions.append(4)
+        elif n[1] - c[1] == 1 and n[0] - c[0] == -1:
+            directions.append(5)
+        elif n[1] == c[1] and n[0] - c[0] == -1:
+            directions.append(6)
+        elif n[1] - c[1] == -1 and n[0] - c[0] == -1:
+            directions.append(7)
+        else:
+            print "fuck"
+        c = n
+    print directions
+
+# sx and sy are the start coordinates, ex and ey the end
+# Returns a list of directions to take
+def AStar(oGrid, sx, sy, ex, ey):
+    h, w = oGrid.shape
+    closedSet = set()
+    openSet = set()
+    openSet.add((sx, sy))
+    cameFrom = {}
+    gScore = {}
+    gScore[(sx, sy)] = 0
+    fScore = []
+    heapq.heappush(fScore, (dist(sx, sy, ex, ey), (sx, sy)))
+    while openSet: # while not empty
+        cx, cy = heapq.heappop(fScore)[1]
+        if (cx, cy) == (ex, ey):
+            return reconstructPath(cameFrom, (cx, cy))
+        openSet.remove((cx, cy))
+        closedSet.add((cx, cy))
+        for i in [cx - 1, cx, cx + 1]:
+            for j in [cy - 1, cy, cy + 1]:
+                if (i, j) in closedSet:
+                    continue
+                if not (i >= 0 and i < w and j >= 0 and j < h and oGrid[j, i] == 1):
+                    continue
+                if (i, j) not in openSet:
+                    openSet.add((i, j))
+                tentativegScore = gScore[(cx, cy)] + 1
+                if tentativegScore >= gScore.get((i, j), float('inf')):
+                    continue
+                cameFrom[(i, j)] = (cx, cy)
+                gScore[(i, j)] = tentativegScore
+                heapq.heappush(fScore, (tentativegScore + dist(i, j, ex, ey), (i, j)))
+    print "Failure"
+    return False
+
+def testAStar():
+    oGrid = np.ones((3,3))
+    AStar(oGrid, 0, 0, 2, 2)
+    oGrid[1,1] = 0
+    AStar(oGrid, 0, 0, 2, 2)
 
 def updateRoboGrid(nav, amap, x, y):
     h, w = amap.oGrid.shape
