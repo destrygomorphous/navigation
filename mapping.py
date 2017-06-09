57ee09c220ddc4374f4f2db0a4f72f87359e6397
destrygomorphous <julie.kew@gmail.com>
2017-06-09 05:32:15 -0700

    better plotting, 3 random explorations

--- mapping.py
+++ mapping.py
@@ -28,7 +28,6 @@ class NavigatorRandom:
 
     def getMove(self, x, y):
         # 0 is up, 1 is up right, 2 is right, etc.
-        # return 2
         return random.randint(0, 7)
 
     def updateOGrid(self, x, y, val):
@@ -38,6 +37,58 @@ class NavigatorRandom:
         plt.imshow(self.oGrid, interpolation='none')
         plt.show()
 
+class NavigatorCurious(NavigatorRandom):
+    def getMove(self, x, y):
+        if self.oGrid[y + 1, x + 1] == 0.5:
+            return 3
+        if self.oGrid[y + 1, x - 1] == 0.5:
+            return 5
+        if self.oGrid[y - 1, x - 1] == 0.5:
+            return 7
+        if self.oGrid[y - 1, x + 1] == 0.5:
+            return 1
+        if self.oGrid[y - 1, x] == 0.5:
+            return 0
+        if self.oGrid[y + 1, x] == 0.5:
+            return 4
+        if self.oGrid[y, x + 1] == 0.5:
+            return 2
+        if self.oGrid[y, x - 1] == 0.5:
+            return 6
+        return random.randint(0, 7)
+
+class NavigatorStraight(NavigatorRandom):
+    def __init__(self, randomness): # 0.3 seems pretty good for randomness
+        # 0=obstacle, 1=free, .5=unknown
+        self.oGrid = None
+        self.viewRadius = 1
+        self.direction = 0
+        self.randomness = randomness
+    def getMove(self, x, y):
+        if random.random() < self.randomness:
+            self.direction = random.randint(0, 3) * 2
+        while True:
+            if self.direction == 0:
+                if self.oGrid[y - 1, x] != 0:
+                    return 0
+                else:
+                    self.direction = 2
+            if self.direction == 2:
+                if self.oGrid[y, x + 1] != 0:
+                    return 2
+                else:
+                    self.direction = 4
+            if self.direction == 4:
+                if self.oGrid[y + 1, x] != 0:
+                    return 4
+                else:
+                    self.direction = 6
+            if self.direction == 6:
+                if self.oGrid[y, x - 1] != 0:
+                    return 6
+                else:
+                    self.direction = 0
+
 def updateRoboGrid(nav, amap, x, y):
     h, w = amap.oGrid.shape
     if nav.viewRadius == 1:
@@ -46,6 +97,11 @@ def updateRoboGrid(nav, amap, x, y):
                 for i in [x - 1, x, x + 1]:
                     if i >= 0 and i < w:
                         nav.updateOGrid(i, j, amap.getVal(i, j))
+
+def fractionExplored(oGrid):
+    unique, counts = np.unique(oGrid, return_counts=True)
+    d = dict(zip(unique, counts))
+    return d[0.5] / float(oGrid.size)
 
 def explore(nav, amap, iterations):
     x = amap.roboStartx
@@ -80,10 +136,13 @@ def explore(nav, amap, iterations):
             y -= 1
             x -= 1
     implot = plt.imshow(nav.oGrid, interpolation='none', cmap='gray', vmin=0, vmax=1)
+    plt.scatter([xPosns[0]], [yPosns[0]], s=80)
     plt.plot(xPosns, yPosns, c='r')
     plt.xlim(0, w - 1)
     plt.ylim(0, h - 1)
+    plt.axis('off')
     plt.show()
+    print fractionExplored(nav.oGrid)
 
 if __name__ == '__main__':
     mapa = OGrid()
@@ -96,6 +155,8 @@ if __name__ == '__main__':
     mapa.setOGrid(arr)
     mapa.roboStartx = 8
     mapa.roboStarty = 8
-    magellan = NavigatorRandom()
+    # magellan = NavigatorRandom()
+    # magellan = NavigatorCurious()
+    magellan = NavigatorStraight(randomness=0.1)
     magellan.initOGrid(mapa)
-    explore(magellan, mapa, 300)
+    explore(magellan, mapa, 80)
